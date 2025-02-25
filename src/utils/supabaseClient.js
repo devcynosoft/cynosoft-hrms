eb2bb8d06f48eff1e5b2242a8fc53964eb300165
OsamaWork1 <osama.workspace1@gmail.com>
2025-02-25 20:31:38 +0500

    remove console

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,11 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const getSupabaseEnv = () => {
-  console.log("NEXT_PUBLIC_NODE_ENV", process.env.NEXT_PUBLIC_NODE_ENV);
-  console.log(
-    "NEXT_PUBLIC_SUPABASE_URL_PRODUCTION",
-    process.env.NEXT_PUBLIC_SUPABASE_URL_PRODUCTION
-  );
   if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
     return {
       url: process.env.NEXT_PUBLIC_SUPABASE_URL_PRODUCTION,
